dabe6523008b01dffcc8ef5b41ba7b5f7170e2c8
katttttsu <katttttsu21@gmail.com>
2024-07-10 21:37:36 +0900

    商品詳細表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:create, :new, :edit, :update, :destroy]
   
   def index
@@ -18,6 +19,24 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+    unless @item.sold? && @item.user_id == current_user.id
+      redirect_to root_path, alert: "この商品の編集は許可されていません。"
+    end
+  end
+  
+  def destroy
+    if @item.sold? && @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path, notice: "商品を削除しました。"
+    else
+      redirect_to root_path, alert: "この商品の削除は許可されていません。"
+    end
+  end
+  
   def sold?
     buyer_id.present?
   end
@@ -27,4 +46,10 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :image, :explanation, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+
 end
