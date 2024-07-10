daf5e18d5cccf474c4c18244f375a4de7a555f45
katttttsu <katttttsu21@gmail.com>
2024-07-10 18:57:00 +0900

    コードレビュー後の修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :new, :edit, :update, :destroy]
   
   def index
-    @items = Item.all
     @items = Item.order(created_at: :asc)
   end
 
