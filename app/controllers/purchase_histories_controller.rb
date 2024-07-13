24f3885fd462b7cf09aa5f299610e6b00dda686e
katttttsu <katttttsu21@gmail.com>
2024-07-14 00:16:14 +0900

    売却済み・未売却の設定

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -9,17 +9,18 @@ class PurchaseHistoriesController < ApplicationController
   end
 
   def create
-    @Purchase_history_address = PurchaseHistoryAddress.new(purchase_history_params)
+    @purchase_history_address = PurchaseHistoryAddress.new(purchase_history_params)
     if @purchase_history_address.valid?
       @purchase_history_address.save
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def purchase_history_params
-    params.require(:donation_address).permit(:postal_code, :prefecture, :city, :house_number, :building_name, :price).merge(user_id: current_user.id)
+    params.require(:purchase_history_address).permit(:postal_code, :prefecture, :city, :house_number, :building_name, :price).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources :items do
-    resources :purchase_histories, only: [:new, :create, :show]
+    resources :purchase_histories, only: [ :new, :create, :show]
   end
   resources :users, only: :create
 end
