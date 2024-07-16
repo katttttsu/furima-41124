7f308ce28ae46fbcf1be32a2105fdcbf834671f0
katttttsu <katttttsu21@gmail.com>
2024-07-17 08:08:50 +0900

    データ整理の為の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_one :purchase_history
+  has_one :purchase_history, dependent: :destroy
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
